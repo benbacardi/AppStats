869007674d14caf5db84e46958f5bff7e48fb1d2
Ben Cardy <ben.cardy@ocado.com>
2022-10-12 15:44:01 +0100

    Initial Commit

---
+++ DeviceInfo/DeviceInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoApp.swift
+//  DeviceInfo
+//
+//  Created by Ben Cardy on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
